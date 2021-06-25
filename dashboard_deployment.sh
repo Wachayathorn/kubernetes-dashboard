f72e61afe7beb4aa1b591759ef8156f3125a2d55
Wachayathorn Singsena <wachayathorn.s@rvconnex.com>
2021-06-25 11:00:23 +0700

    feat: add files

---
+++ dashboard_deployment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.3.1/aio/deploy/recommended.yaml
+kubectl apply -f ./kubernetes_dashboard_user.yaml
+kubectl apply -f ./kubernetes_dashboard_role.yaml
+kubectl apply -f ./kubernetes_dashboard_ingress.yaml
+kubectl apply -f ./kubernetes_dashboard_ingress_class.yaml
